bb58f8193fc6b569f57efed46c8d49e077005ae8
amitark7 <amitkumar1600121@gmail.com>
2023-12-12 00:01:24 +0530

    Update Backend Link

--- client/src/api/AuthRequest.js
+++ client/src/api/AuthRequest.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API=axios.create({baseURL:"http://localhost:5000"})
+const API=axios.create({baseURL:"https://ark-social-media-clone.onrender.com"})
 //const host='http://localhost:5000'
 
 export const LogIn =(formData)=>API.post('/auth/login',formData)
--- client/src/api/UploadRequest.js
+++ client/src/api/UploadRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API=axios.create({baseURL:"http://localhost:5000"})
+const API=axios.create({baseURL:"https://ark-social-media-clone.onrender.com"})
 
 export const uploadImage=(data)=>API.post("/upload",data)
 
